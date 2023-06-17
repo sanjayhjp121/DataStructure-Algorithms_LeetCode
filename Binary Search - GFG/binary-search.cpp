9f65fdd4ab0d5c94df7d68dea64589520d17ef56
Sanjay Kumar <72185623+sanjayhjp121@users.noreply.github.com>
2023-06-17 20:54:52 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -15,11 +15,11 @@ class Solution {
         
         int s = 0;
         int e = n - 1;
-        int mid = s + (e-s)/2;
+        
         
         while(s<=e){
             
-            
+            int mid = s + (e-s)/2;
             if(k == arr[mid]){
                 return mid;
             }
